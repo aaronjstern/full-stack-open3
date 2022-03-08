8600a643c7ea67695081043b9312f98541e1cbd1
Aaron Stern <aaronjstern1@gmail.com>
2022-03-08 18:41:55 -0500

    exercise 3.7

--- phonebookBackEnd/index.js
+++ phonebookBackEnd/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 let persons = [
   {
@@ -24,6 +25,7 @@ let persons = [
   },
 ];
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/api/persons", (request, response) => {

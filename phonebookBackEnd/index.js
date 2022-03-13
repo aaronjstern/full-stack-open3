51e613877df7992d0a96462f559579f46def70a2
Aaron Stern <aaronjstern1@gmail.com>
2022-03-13 17:34:57 -0400

    exercises 3.15-16

--- phonebookBackEnd/index.js
+++ phonebookBackEnd/index.js
@@ -6,29 +6,6 @@ const cors = require("cors");
 const Person = require("./models/person");
 const mongoose = require("mongoose");
 
-let personsHardCode = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors());
@@ -60,9 +37,12 @@ app.post("/api/persons", (request, response) => {
       name: body.name,
       number: body.number,
     });
-    person.save().then((savedPerson) => {
-      response.json(savedPerson);
-    });
+    person
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
   }
 });
 
@@ -78,10 +58,27 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((p) => p.id !== id);
-  response.status(204).end();
+  const id = request.params.id;
+  Person.deleteOne({ _id: id }).then((result) => {
+    response.status(204).end();
+  });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status.send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return response.status.send({ error: "malformed id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
